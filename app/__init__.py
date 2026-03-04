dbba47204463b6cabd60530a1aa20d9c71c5aef5
Lukáš Hotový <125402853+Lukynka22@users.noreply.github.com>
2026-03-04 17:39:56 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,8 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DATABASE}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://student13:spsnet@dbs.spskladno.cz/vyuka13"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SECRET_KEY'] = SECRET_KEY
 
     db.init_app(app)

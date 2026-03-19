14b13c39a01efa1bf4e637cc25eff7e4623ad146
Lukáš Hotový <125402853+Lukynka22@users.noreply.github.com>
2026-03-19 16:53:39 +0100

    Update cesty.py

--- app/views/cesty.py
+++ app/views/cesty.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, url_for
 from app.models import Product
 main_bp = Blueprint('main', __name__)
 
@@ -8,7 +8,36 @@ def index():
 @main_bp.route('/about')
 def about():
     return render_template('about.html')
+
+
+
+
 @main_bp.route('/hodinky')
 def hodinky():
-    produkty = Product.query.all()
-    return render_template('hodinky.html', produkty=produkty)
+
+    search = request.args.get("q")
+    category = request.args.get("category")
+
+    query = Product.query
+
+    if search:
+        query = query.filter(Product.nazev.ilike(f"%{search}%"))
+
+    if category:
+        query = query.filter(Product.kategorie == category)
+
+    produkty = query.all()
+
+    breadcrumbs = [
+        ("Domů", url_for("main.index")),
+        ("Hodinky", url_for("main.hodinky"))
+    ]
+
+    if category:
+        breadcrumbs.append((category, None))
+
+    return render_template(
+        "hodinky.html",
+        produkty=produkty,
+        breadcrumbs=breadcrumbs
+    )
